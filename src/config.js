e72bccf6440665eb9c90162935abe31604822750
halfpasta <christian.george360@gmail.com>
2020-06-13 23:00:23 -0400

    initial calls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://christiangeorge@localhost/movie-forum-capstone-server',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://christiangeorge@localhost/movie-forum-capstone-server',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fantasy-data',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/fantasydata',
   JWT_SECRET: process.env.JWT_SECRET 
 }
 
