a9eaa11d6802ac8f1d3f051038870f4b02f953f4
halfpasta <christian.george360@gmail.com>
2020-06-19 12:17:33 -0400

    changed to local db

--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//   PORT: process.env.PORT || 8000,
+//   NODE_ENV: process.env.NODE_ENV || 'development',
+//   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fantasy-data',
+//   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/fantasydata',
+//   JWT_SECRET: process.env.JWT_SECRET 
+// }
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fantasy-data',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fantasy-data-server',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/fantasydata',
   JWT_SECRET: process.env.JWT_SECRET 
 }
