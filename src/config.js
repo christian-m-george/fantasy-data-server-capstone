4a6f0f2d2deeb6735df9128b89bb3b830dd5cf34
halfpasta <christian.george360@gmail.com>
2020-06-19 12:18:59 -0400

    change to config

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fantasy-data-server',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/fantasydata',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://christiangeorge@localhost/fantasy-data-server',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://christiangeorge@localhost/fantasydata',
   JWT_SECRET: process.env.JWT_SECRET 
 }
 
