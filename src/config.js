9bbc88542bde3cf6d2aa609366faeb63f33d0c0f
halfpasta <christian.george360@gmail.com>
2020-09-21 22:54:04 -0400

    test changes

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'DATABASE_URL=postgres://lztsoqxstgfzdq:bd31be8b59760ea56c93ab12152f93df71369c8fb292b2f49180fe597c7ff9fe@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d4if01mt6cjg7g',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/fantasydata',
-  JWT_SECRET: process.env.JWT_SECRET 
+  JWT_SECRET: process.env.JWT_SECRET,
 }
 
 // module.exports = {
