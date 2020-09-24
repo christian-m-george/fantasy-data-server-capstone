42134fb97023a0e6eb70ffeee09e570be2dbe8f9
halfpasta <christian.george360@gmail.com>
2020-09-24 16:47:27 -0400

    access control origin vercel

--- src/app.js
+++ src/app.js
@@ -19,10 +19,14 @@ const morganOption = (NODE_ENV === 'production')
     : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors({
-    'Access-Control-Allow-Origin': '*'
-}))
+app.use(cors())
 app.use(helmet())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://fantasy-data-react-capstone.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.get('/', (req, res) => {
